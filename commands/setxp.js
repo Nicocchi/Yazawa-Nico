8e6f8a91a3e1d74936afa9eeec5f6d176a552e1d
Nicocchi <nicocchilover@gmail.com>
2018-10-30 21:24:01 -0400

    initial commit

---
+++ commands/setxp.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+
+//  Description: Display the user\' avatar.
+//  Usage: prefix arg1 arg2
+exports.run = async (client, message, args) => {
+
+    client.settings.set(message.author.id, 1, 'level');
+    client.settings.set(message.author.id, args[0], 'xp');
+
+
+    message.channel.send(`${message.author.tag} has set current xp to ${args[0]}`);
+};
+
+exports.conf = {
+    enabled: 'true',
+    guildOnly: 'false',
+    aliases: '[]',
+    permLevel: 'Bot Owner'
+};
+
+exports.help = {
+    name: 'setxp',
+    category: 'Administration',
+    description: 'Add\'s experience',
+    usage: 'addxp [amount]'
+};
