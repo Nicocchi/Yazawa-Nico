e9e0c588162d4a20d6a1c1b602d11b2e1db4f303
Jeremy Boggs <nicocchi@localhost.localdomain>
2018-11-04 10:28:34 -0500

    updated greet messages

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,14 +2,17 @@
 
 module.exports = (client, member) => {
     // Load the guild's settings
-    const settings = client.getSettings(member.guild.id);
+    const defaults = client.config.defaultSettings;
+    if (!client.settings.has(member.guild.id)) client.settings.set(member.guild.id, defaults);
+
+    const settings = client.settings.get(member.guild.id);
 
     // If welcome is off, don't proceed
     if (settings.welcomeEnabled !== 'true') return;
 
     // Replace the placeholders in the welcome message with actual data
-    const welcomeMessage = settings.welcomeMessage.replace('{{user}}', member.user.tag);
+    const welcomeMessage = settings.welcomeMessage.replace('{{user}}', `<@${member.user.id}>`);
 
     // Send the welcome message to the welcome channel
-    member.guild.channels.find(c => c.name === settings.welcomeChannel).send(welcomeMessage).catch(console.error);
+    member.guild.channels.find(c => c.id === settings.welcomeChannel).send(welcomeMessage).catch(console.error);
 };
