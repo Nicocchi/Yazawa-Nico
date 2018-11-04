b7d0315119b253ecd720fb830cc220da8d8115e1
Nicocchi <nicocchilover@gmail.com>
2018-11-04 11:18:25 -0500

    kick command complete

---
+++ commands/kick.js
@@ -0,0 +1,44 @@
+//  Description: kick a mentioned user.
+//  Usage: ban <user> <reason>
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args, level) => {
+    const defaults = client.config.defaultSettings;
+    if (!client.settings.has(message.guild.id))
+        client.settings.set(message.guild.id, defaults);
+    const settings = client.settings.get(message.guild.id);
+
+    if(!message.member.hasPermission('MANAGE_MEMBERS')) return;
+
+    let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!user) return message.channel.send('Couldn\'t find user');
+    let reason = args.join(' ').slice(22);
+    if(!message.member.hasPermission('MANAGE_MEMBERS')) return message.channel.send('Can\'t kick user because you don\'t have permission');
+
+    let embed = new Discord.RichEmbed()
+        .setDescription('Kick')
+        .setColor('#FF4D9C')
+        .addField('Kicked User', `${user} with ID ${user.id}`)
+        .addField('Kicked By', `<@${message.author.id}> with ID ${message.author.id}`)
+        .addField('Kicked In', message.channel)
+        .addField('Time', message.createdAt)
+        .addField('Reason', reason);
+
+    user.send(embed);
+    message.guild.member(user).kick(reason);
+    message.guild.channels.find(c => c.id === settings.modLogChannel).send(embed).catch(console.error);
+};
+
+exports.conf = {
+    enabled: "true",
+    guildOnly: "false",
+    aliases: '["h", "help"]',
+    permLevel: "Administrator"
+};
+
+exports.help = {
+    name: "kick",
+    category: "System",
+    description: "Kicks a mentioned user",
+    usage: "help [command]"
+};
