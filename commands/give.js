d9d1c0a0258d7061b5f9c37ece41375afa3618b3
Nicocchi <nicocchilover@gmail.com>
2018-12-04 20:36:54 -0500

    give command added

---
+++ commands/give.js
@@ -0,0 +1,45 @@
+const Discord = require("discord.js");
+//  Description: Give a cucumber to someone
+//  Usage: cucumber arg1
+exports.run = async (client, message, args, level) => {
+  let user =
+    message.guild.member(message.mentions.users.first()) ||
+    message.guild.members.get(args[0]);
+
+  client.logger.log(args);
+
+  if (!user) {
+    let item = args[0];
+    if (!item)
+      return message.reply(
+        "Command usage => <prefix>give <user> [item] (ex: !give <user> item"
+      );
+    var msg = `You got a ${item} from yourself\n\n(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ ${item}`;
+  } else {
+    let item = args[1];
+    if (!item) {
+      message.reply(
+        "Command usage => <prefix>give <user> [item] (ex: !give <user> item"
+      );
+      return;
+    }
+    var msg = `${user.user}, you got a ${item} from ${
+      message.author.username
+    }\n\n(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ ${item}`;
+  }
+  message.channel.send(msg);
+};
+
+exports.conf = {
+  enabled: "true",
+  guildOnly: "false",
+  aliases: "[]",
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "give",
+  category: "Fun",
+  description: "Give an item to someone",
+  usage: "give <user>"
+};
