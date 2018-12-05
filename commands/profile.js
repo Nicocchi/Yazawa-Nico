9e7ab4e55c9575726fec13760cf402007545a3a2
Nicocchi <nicocchilover@gmail.com>
2018-12-04 22:11:24 -0500

    fixed profile to show current xp/next level xp amount and fixed emoji render

--- commands/profile.js
+++ commands/profile.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js");
 
 //  Description: Display User Profile Stats
 //  Usage: prefix arg1 arg2
@@ -7,52 +7,60 @@ exports.run = async (client, message, args, level) => {
   const settings = message.settings;
   // const defaults = client.config.defaultUserSettings;
   const defaults = {
-    'id': message.author.id,
-    'username': `${message.author.username}`,
-    'points': 0,
-    'xp': 0,
-    'level': 1,
-    'daily': 'time', // Time of daily
-    'dailyB': 'true',
-    'isMuted': 'false',
-    'afk': 'false',
-    'afkMessage': 'I am AFK right now.',
-    'isRPS': 'false',
-    'isRPSGamble': 'false',
-    'marriageProposals': [],
-    'sentMarriageProposals': [],
-    'marriages': [],
-    'marriageSlots': 0,
-    'isBuyingSlot': 'false'
+    id: message.author.id,
+    username: `${message.author.username}`,
+    points: 0,
+    xp: 0,
+    level: 1,
+    daily: "time", // Time of daily
+    dailyB: "true",
+    isMuted: "false",
+    afk: "false",
+    afkMessage: "I am AFK right now.",
+    isRPS: "false",
+    isRPSGamble: "false",
+    marriageProposals: [],
+    sentMarriageProposals: [],
+    marriages: [],
+    marriageSlots: 0,
+    isBuyingSlot: "false"
   };
   const overrides = client.settings.get(message.author.id);
-  const niiEmote = client.emojis.find('name', 'niconii');
-  const niiLoveEmote = client.emojis.find('name', 'nicolove');
-  if (!client.settings.has(message.author.id)) client.settings.set(message.author.id, defaults);
+  const niiEmote = client.emojis.find("name", "niconii");
+  const niiLoveEmote = client.emojis.find("name", "nicolove");
+  if (!client.settings.has(message.author.id))
+    client.settings.set(message.author.id, defaults);
 
   let embed = new Discord.RichEmbed()
     .setTitle(`${message.author.username}'s Global Profile`)
     .setTimestamp()
-    .setColor('#FF4D9C')
+    .setColor("#FF4D9C")
     .setThumbnail(message.author.displayAvatarURL)
-    .setFooter(`Server member since ${message.member.joinedAt}`, message.author.displayAvatarURL)
-    .addField('Level & EXP', `${overrides.level}[${overrides.xp}/${overrides.xp}]`, true)
-    .addField('Nico Nii\'s', `${overrides.points} ${niiEmote}`, true)
-    .addField(`Marriages <:nicolove:451873491130646528>`, `None`);
+    .setFooter(
+      `Server member since ${message.member.joinedAt}`,
+      message.author.displayAvatarURL
+    )
+    .addField(
+      "Level & EXP",
+      `${overrides.level}[${overrides.xp}/${overrides.level * 300}]`,
+      true
+    )
+    .addField("Nico Nii's", `${overrides.points} ${niiEmote}`, true)
+    .addField(`Marriages <:nicolove:506940178246533120>`, `None`);
 
   message.channel.send(embed);
 };
 
 exports.conf = {
-  enabled: 'true',
-  guildOnly: 'false',
-  aliases: '[]',
-  permLevel: 'User'
+  enabled: "true",
+  guildOnly: "false",
+  aliases: "[]",
+  permLevel: "User"
 };
 
 exports.help = {
-  name: 'profile',
-  category: 'Miscelaneous',
-  description: 'Display User Profile Stats',
-  usage: 'profile'
+  name: "profile",
+  category: "Miscelaneous",
+  description: "Display User Profile Stats",
+  usage: "profile"
 };
