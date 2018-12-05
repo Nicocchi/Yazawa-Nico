242c5147fa4d7da677b8edbe8dba73b674c25941
Nicocchi <nicocchilover@gmail.com>
2018-12-05 13:11:41 -0500

    8ball command added

---
+++ commands/8ball.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+//  Description: Ask the 8ball
+//  Usage: 8ball *arg1
+exports.run = async (client, message, args, level) => {
+  // Join the arguments into an array and then split them with the |
+  let options = args;
+  client.logger.log(options);
+  let replies = [
+    "Yes",
+    "No",
+    "I do not know",
+    "Ask again later",
+    "Ni-Nico Nii?",
+    "Maybe",
+    "Hmm, appears to be true..."
+  ];
+  if (!options || options.length === 0) {
+    return message.reply("Please ask a question~");
+  }
+
+  // Randomly generate an index
+  let i = client.randomNumber(0, replies.length - 1);
+  message.reply(`${replies[i]}`);
+};
+
+exports.conf = {
+  enabled: "true",
+  guildOnly: "false",
+  aliases: "[]",
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "8ball",
+  category: "Fun",
+  description: "Ask the 8ball",
+  usage: "8ball [question]"
+};
