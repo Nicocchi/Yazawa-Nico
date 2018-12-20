946e0d290c8df61583ebebb32c6a23b5091e5f20
Nicocchi <nicocchilover@gmail.com>
2018-12-20 13:25:45 -0500

    updated daily command

--- commands/daily.js
+++ commands/daily.js
@@ -19,14 +19,18 @@ exports.run = async (client, message, args, level) => {
   // Get the user's love gems
   let loveGems = settings.points;
 
+  const dff = moment(daily).utc();
+
   // If the user is not using the command for the first time, set the difference for comparison
-  if (daily != "time") diff = daily.diff(m, "hours");
+  if (daily != "time") {
+    diff = dff.diff(m, "hours");
+  }
+
 
   // Compare the dates and add the love gems, if already claimed, return message stating user has already claimed
   // If the user is using the command for the first time or 24 hours have passed since their last time using the
   // command
   if (daily === "time" || diff === 24) {
-    client.logger.log("Adding Love Gems");
     daily = m;
     loveGems += 200;
     client.settings.set(message.author.id, daily, "daily");
