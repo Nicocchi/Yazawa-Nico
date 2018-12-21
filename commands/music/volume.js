ec80ab5d7547659305e4161d33e0298492aadf35
Nicocchi <nicocchilover@gmail.com>
2018-12-21 18:08:36 -0500

    volume modification added

---
+++ commands/music/volume.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const ytdl = require("ytdl-core");
+
+//  Description: View and change the volume
+//  Usage: prefix arg1
+
+exports.run = async (client, message, args, level) => {
+  const serverQueue = client.queue.get(message.guild.id);
+  if (!serverQueue) return message.channel.send("There is nothing playing.");
+  if (!args[0])
+    return message.channel.send(`The current volume is ${serverQueue.volume}`);
+  serverQueue.volume = args[0];
+  serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 5);
+
+  message.channel.send(`Volume set to ${args[0]}`);
+};
+
+exports.conf = {
+  enabled: "true",
+  guildOnly: "false",
+  aliases: ["vol"],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "volume",
+  category: "Music",
+  description: "View and change the volume",
+  usage: "volume"
+};
