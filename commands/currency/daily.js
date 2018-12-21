d349ddc208ed82483bfe0f1848d042c2bd6f6c3a
Nicocchi <nicocchilover@gmail.com>
2018-12-21 15:28:50 -0500

    removed logging

--- commands/currency/daily.js
+++ commands/currency/daily.js
@@ -20,15 +20,11 @@ exports.run = async (client, message, args, level) => {
   let loveGems = settings.points;
 
   const dff = moment(daily).utc();
-  client.logger.log(m);
-  client.logger.log(dff);
 
   // If the user is not using the command for the first time, set the difference for comparison
   if (daily != "time") {
     diff = m.diff(dff, "hours");
   }
-
-  client.logger.log(diff);
 
 
   // Compare the dates and add the love gems, if already claimed, return message stating user has already claimed
