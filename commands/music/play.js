4222adf822835ad85e92c396a22a5891ef0b64c1
Nicocchi <nicocchilover@gmail.com>
2018-12-21 17:32:41 -0500

    queue system implemented for music

--- commands/music/play.js
+++ commands/music/play.js
@@ -5,6 +5,8 @@ const ytdl = require("ytdl-core");
 //  Usage: prefix arg1
 
 exports.run = async (client, message, args, level) => {
+  const serverQueue = client.queue.get(message.guild.id);
+
   const voiceChannel = message.member.voiceChannel;
   if (!voiceChannel)
     return message.reply(`You need to be in a voice channel to play music`);
@@ -15,30 +17,48 @@ exports.run = async (client, message, args, level) => {
       "I cannot to voice channel, please make " +
         "sure I have the proper permissions."
     );
+
   if (!permissions.has("SPEAK"))
     return message.channel.send(
       "I cannot to speack in this voice channel, " +
         "please make sure I have the proper permissions."
     );
 
-  try {
-    var connection = await voiceChannel.join();
-  } catch (e) {
-    client.logger.error(e);
-    return message.channel.send("I could not join the voice channel.");
-  }
+  const songInfo = await ytdl.getInfo(args[0]);
+  const song = {
+    title: songInfo.title,
+    url: songInfo.video_url,
+    thumbnail: songInfo.thumbnail_url
+  };
 
-  const dispatcher = connection
-    .playStream(ytdl(args[0]))
-    .on("end", () => {
-      client.logger.log("Song Ended!");
-      voiceChannel.leave();
-    })
-    .on("Error", e => {
+  if (!serverQueue) {
+    const queueConstruct = {
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      connection: null,
+      songs: [],
+      volume: 5,
+      playing: true
+    };
+    client.queue.set(message.guild.id, queueConstruct);
+
+    queueConstruct.songs.push(song);
+
+    try {
+      var connection = await voiceChannel.join();
+      queueConstruct.connection = connection;
+      client.play(message.guild, queueConstruct.songs[0]);
+    } catch (e) {
       client.logger.error(e);
-    });
-
-  dispatcher.setVolumeLogarithmic(5 / 5);
+      client.queue.delete(message.guild.id);
+      return message.channel.send("I could not join the voice channel.");
+    }
+  } else {
+    serverQueue.songs.push(song);
+    message.channel.send(`**${song.title}** has been added to the queue!`);
+    client.logger.log(`${song.title} has been added to the queue!`);
+    client.logger.log(serverQueue.songs);
+  }
 };
 
 exports.conf = {
