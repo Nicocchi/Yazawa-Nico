e2c36358d1bc72097b47f063012ba11058acf155
Nicocchi <nicocchilover@gmail.com>
2018-12-22 05:44:23 -0500

    updated channel create and delete

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -3,19 +3,19 @@ const Discord = require("discord.js");
 
 module.exports = (client, channel) => {
   // Load the guild's settings
-  const defaults = client.config.defaultSettings;
-  if (!client.settings.has(channel.guild.id))
-    client.settings.set(channel.guild.id, defaults);
-
-  const settings = client.settings.get(channel.guild.id);
-
-  // If no modLogChannel, don't proceed
-  if (!settings.modlog) return;
-  const modLogChannel = channel.guild.channels.find(
-    c => c.id === settings.modLogChannel
-  );
-
-  if (!modLogChannel) return;
+  // const defaults = client.config.defaultSettings;
+  // if (!client.settings.has(channel.guild.id))
+  //   client.settings.set(channel.guild.id, defaults);
+  //
+  // const settings = client.settings.get(channel.guild.id);
+  //
+  // // If no modLogChannel, don't proceed
+  // if (!settings.modlog) return;
+  // const modLogChannel = channel.guild.channels.find(
+  //   c => c.id === settings.modLogChannel
+  // );
+  //
+  // if (!modLogChannel) return;
   // try {
   //   let embed = new Discord.RichEmbed()
   //     .setDescription(`Channel Created: <#${channel.name}>`)
