507b8d046773b9f179a91c39daee0efc40931fd4
Nicocchi <nicocchilover@gmail.com>
2019-01-13 16:38:54 -0500

    updated music commands

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,12 +1,13 @@
 const { Discord, Util } = require("discord.js");
 const ytdl = require("ytdl-core");
-
 //  Description: Play a song from YouTube
 //  Usage: prefix arg1
 
 exports.run = async (client, message, args, level) => {
+  // Create server queue
   const serverQueue = client.queue.get(message.guild.id);
 
+  // Check channel and permissions
   const voiceChannel = message.member.voiceChannel;
   if (!voiceChannel)
     return message.reply(`You need to be in a voice channel to play music`);
@@ -24,6 +25,7 @@ exports.run = async (client, message, args, level) => {
         "please make sure I have the proper permissions."
     );
 
+  // Begin searching
   const searchString = args.join(" ");
   const url = args.join(" ");
   let waited = false;
@@ -45,26 +47,25 @@ exports.run = async (client, message, args, level) => {
       var video = await client.youtube.getVideo(url);
       waited = true;
     } catch (e) {
+      // Search videos
       try {
-        var videos = await client.youtube.searchVideos(searchString, 10);
+        client.logger.log("SEARCHING VIDEOS");
+        client.logger.log(searchString);
+
+        const results = await client.youtube.searchVideos(searchString, 10);
+
         const nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
-        videos.map(song => console.log(song));
-
-        const response = await client.awaitReply(
-          message, `
-  __**Search Results:**__
-  ${videos.map((song, i) => `[${++i}] - ${song.title}`).join("\n  ")}
-
-  Please select a song ~Nico~
-  `,
-          30000
-        );
+        const response = await client.awaitReply(message, `
+          __**Search Results:**__
+          ${results.map((song, i) => `\n[${i <= 8 ? `0${++i}` : `${++i}`}] - **${song.title}**`).join("")}
+          \nPlease select a song ~Nico~
+          `, 30000);
 
         if (nums.includes(response)) {
-          const index = parseInt(response, 10);
+          const ind = parseInt(response, 10);
 
-          var video = videos[index - 1];
+          var video = results[ind - 1];
           waited = true;
         }
       } catch (e) {
