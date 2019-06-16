87467125ac0d26d9e4fa29eee95cc6f4d100a060
Jeremy Boggs <nicocchilover@gmail.com>
2019-06-16 19:27:12 -0400

    updated profile command to produce image

--- commands/info/profile.js
+++ commands/info/profile.js
@@ -1,71 +1,180 @@
 const Discord = require("discord.js");
+const axios = require('axios');
+const Canvas = require('canvas');
+const { Image } = require('canvas');
 
 //  Description: Display User Profile Stats
 //  Usage: prefix arg1 arg2
 
+// Pass the entire Canvas object because you'll need to access its width, as well its context
+const applyText = async (canvas, text, fntSize, weight = "normal") => {
+	const ctx = canvas.getContext('2d');
+
+	// Declare a base size of the font
+	let fontSize = fntSize;
+
+	do {
+		// Assign the font to the context and decrement it so it can be measured again
+		ctx.font = `${weight} ${fontSize -= 10}px sans-serif`;
+		// Compare pixel width of the text to the canvas minus the approximate avatar size
+	} while (ctx.measureText(text).width > canvas.width - 300);
+
+	// Return the result to use in the actual canvas
+	return ctx.font;
+};
+
 exports.run = async (client, message, args, level) => {
-  const settings = message.settings;
-  // const defaults = client.config.defaultUserSettings;
-  const defaults = {
-    id: message.author.id,
-    username: `${message.author.username}`,
-    points: 0,
-    xp: 0,
-    level: 1,
-    daily: "time", // Time of daily
-    isMuted: false,
-    afk: false,
-    afkMessage: "I am AFK right now.",
-    isRPS: false,
-    isRPSGamble: false,
-    marriageProposals: [],
-    sentMarriageProposals: [],
-    marriages: [],
-    marriageSlots: 5,
-    isBuyingSlot: false
-  };
+  const res = await axios.post('http://localhost:8000/users/profile', {'discord_id': message.author.id});
+  console.log('res =>', res.data);
+  // const bf = Buffer.from(profile.profileImage, 'binary').toString('base64');
+  const profile = res.data.profile.local;
 
-  if (!client.settings.has(message.author.id))
-    client.settings.set(message.author.id, defaults);
+  const canvas = Canvas.createCanvas(700, 700);
+  const ctx = canvas.getContext('2d');
 
-  const overrides = client.settings.get(message.author.id);
+  // Draw the background
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  const marriageIds = overrides.marriages;
-  let marriages = [];
-  let marriageString = "None";
+  // Dropshadow
+  ctx.shadowColor = '#898';
+  ctx.shadowBlur = 20;
+  ctx.shadowOffsetX = -20;
+  ctx.shadowOffsetY = 20;
 
-  if (marriageIds.length > 0) {
-    marriageIds.forEach(usrId => {
-      client.logger.log(usrId);
-      let usrSettings = client.settings.get(usrId);
-      client.logger.log(usrSettings);
-      if (usrSettings) {
-        marriages.push(usrSettings.username);
-      }
-    });
+  // const background = await Canvas.loadImage(`data:image/png;base64,${bf}`);
+  const bgImage = await Canvas.loadImage(__dirname + '/wallpaper.png');
+  ctx.drawImage(bgImage, 10, 10, canvas.width - 20, canvas.height - 20);
 
-    marriageString = marriages.join("\n");
+  // Reset the blur effect
+  ctx.shadowBlur = 0;
+  ctx.shadowBlur = 0;
+  ctx.shadowOffsetX = 0;
+  ctx.shadowOffsetY = 0;
+
+  // Header
+  ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
+  ctx.fillRect(10, 200, 680, 150);
+
+  // Left column
+  ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
+  ctx.fillRect(10, 350, 200, 340);
+
+  // Right column
+  ctx.fillStyle = "rgba(255, 255, 255, 0.7)";
+  ctx.fillRect(210, 350, 500, 340);
+
+  // Draw avatar
+  const avatar = await Canvas.loadImage(message.author.displayAvatarURL);
+  ctx.drawImage(avatar, 20, 180, 180, 180 );
+
+  // Draw user's username
+  ctx.font = await applyText(canvas, message.author.username, 80);
+  ctx.fillStyle = '#ffffff';
+  ctx.fillText(message.author.username, 260, 270);
+
+  // Draw user's level
+  ctx.font = await applyText(canvas, "Level", 50);
+  ctx.fillStyle = '#ffffff';
+  ctx.fillText("LEVEL", 440, 340);
+
+  ctx.font = await applyText(canvas, "999", 50, "bold");
+  ctx.fillStyle = '#ffffff';
+  ctx.fillText(profile.level, 580, 340);
+
+  // Draw the XP Gauge
+  // Background
+  ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
+  ctx.fillRect(250, 370, 400, 50);
+
+  // Calculate value for front bar
+  const xp = profile.experience;
+  const maxXP = 85800;
+  const perc = (xp / maxXP) * 100;
+  console.log(perc);
+  const value = Math.floor(perc * 400) / 100
+  console.log(value);
+
+  // Front bar
+  ctx.fillStyle = "rgba(0, 118, 210, 0.7)";
+  ctx.fillRect(250, 370, value, 50);
+
+  // XP Text
+  ctx.font = await applyText(canvas, `XP 99999 / 99999`, 40);
+  ctx.fillStyle = '#ffffff';
+  ctx.textAlign = "end";
+  ctx.fillText(`XP ${xp} / ${maxXP}`, 600, 405);
+
+  // Draw Cards Text
+  ctx.font = await applyText(canvas, `Cards`, 50);
+  ctx.fillStyle = '#000000';
+  ctx.textAlign = "start";
+  ctx.fillText(`Cards`, 230, 480);
+
+  ctx.font = await applyText(canvas, `999999`, 50);
+  ctx.fillStyle = '#000000';
+  ctx.fillText(`250`, 600, 480);
+
+  // Draw Love Gems Text
+  ctx.font = await applyText(canvas, `Love Gems`, 50);
+  ctx.fillStyle = '#000000';
+  ctx.fillText(`Love Gems`, 230, 540);
+
+  ctx.font = await applyText(canvas, `999999`, 50);
+  ctx.fillStyle = '#000000';
+  ctx.textAlign = "end";
+  ctx.fillText(profile.loveGems, 675, 540);
+
+  function fragmentText(text, maxWidth) {
+    var words = text.split(' '),
+        lines = [],
+        line = "";
+    if (ctx.measureText(text).width < maxWidth) {
+        return [text];
+    }
+    while (words.length > 0) {
+        while (ctx.measureText(words[0]).width >= maxWidth) {
+            var tmp = words[0];
+            words[0] = tmp.slice(0, -1);
+            if (words.length > 1) {
+                words[1] = tmp.slice(-1) + words[1];
+            } else {
+                words.push(tmp.slice(-1));
+            }
+        }
+        if (ctx.measureText(line + words[0]).width < maxWidth) {
+            line += words.shift() + " ";
+        } else {
+            lines.push(line);
+            line = "";
+        }
+        if (words.length === 0) {
+            lines.push(line);
+        }
+    }
+    return lines;
   }
 
-  let embed = new Discord.RichEmbed()
-    .setTitle(`${message.author.username}'s Global Profile`)
-    .setTimestamp()
-    .setColor("#FF4D9C")
-    .setThumbnail(message.author.displayAvatarURL)
-    .setFooter(
-      `Server member since ${message.member.joinedAt}`,
-      message.author.displayAvatarURL
-    )
-    .addField(
-      "Level & EXP",
-      `${overrides.level} [${overrides.xp}/${overrides.level * 600}]`,
-      true
-    )
-    .addField("Love Gems", `${overrides.points}`, true)
-    .addField(`Marriages <:nicolove:506940178246533120>`, marriageString, true)
-    .addField(`Marriage Slots`, `${overrides.marriageSlots}/10`, true);
+  // Draw Info Text
+  const text = `I am the number one idol in the universe! This is getting a bit too long isn't it? Isn't it?`;
+  const txt = text.slice(0, 85)
+  // ctx.font = await applyText(canvas, text, 50);
+  ctx.font = "30px sans-serif"
+  ctx.fillStyle = '#000000';
+  ctx.textAlign = "start";
 
-  message.channel.send(embed);
+  const lines = fragmentText(txt, 430);
+  console.log(lines);
+
+  lines.forEach(function(line, i) {
+    ctx.fillText(line, 230, (i + 1) * 30 + 560);
+  })
+  
+
+  const attachment = new Discord.Attachment(canvas.toBuffer(), 'user_profile.png');
+
+  message.channel.send(attachment);
+    
 };
 
 exports.conf = {
