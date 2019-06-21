b96c8fcf4917c12ac4dd7d385124015862da50d9
Jeremy Boggs <nicocchilover@gmail.com>
2019-06-20 21:19:07 -0400

    clear and warn commands updated

--- commands/moderation/warn.js
+++ commands/moderation/warn.js
@@ -1,80 +1,57 @@
-//  Description: warn a mentioned user.
-//  Usage: warn <user> <reason>
 const Discord = require("discord.js");
+const axios = require('axios');
 const fs = require('fs');
 const ms = require('ms');
-// let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
+//  Description: warn a mentioned user.
+//  Usage: warn <user> <reason>
 exports.run = async (client, message, args, level) => {
-    const defaults = client.config.defaultSettings;
-    if (!client.settings.has(message.guild.id))
-        client.settings.set(message.guild.id, defaults);
-    const settings = client.settings.get(message.guild.id);
+    console.log("RUN")
+    // Get mentioned user, if none, return error
+    let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!user) return message.channel.send(`:x: Unable to warn user **undefined** due to an error.\n \`You must mention a user to warn\``);
 
-    if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('Can\'t warn user because you don\'t have permission');
+    // Check if author has proper permissions
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`:x: Unable to warn user **${user.user.username}** due to an error.\n \`You do not have the required permission: 'ADMINISTRATOR'\``);
 
-    let user = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-    if(!user) return message.reply('Could not find user');
-
-    if(user.hasPermission('ADMINISTRATOR')) return message.channel.send('Can\'t warn user because you don\'t have permission');
+    // Get reason. If none set, set default reason
     let reason = args.join(' ').slice(22);
-    if(!reason) return message.reply('Need to specify a reason');
+    if (reason === '') reason = 'No reason given'
 
+    try {
+        const res = await axios.post('http://localhost:8000/guilds/set-warn', {'discord_id': message.guild.id, 'name': message.guild.name, warnUser: user.user.id, warning: reason });
 
-    let warnings = settings.warnings;
-
-    if(Object.keys(warnings).length === 0 && warnings.constructor === Object) {
-        const defWarn = {
-            [user.id] : {"warns": 0}
-        }
-        client.settings.set(message.guild.id, defWarn, 'warnings');
-    }
-
-    let warns = settings.warnings;
-
-    if(!warns[user.id]) {
-        const defWarn = {
-            ...warnings,
-            [user.id] : {"warns": 1}
-        }
-        client.settings.set(message.guild.id, defWarn, 'warnings');
-    } else {
-        let numb = warns[user.id].warns;
-        const defWarn = {
-            ...warnings,
-            [user.id] : {"warns" : ++numb}
-        }
-        client.settings.set(message.guild.id, defWarn, 'warnings');
-    }
-
-    let newWarns = settings.warnings;
-    let numberW = newWarns[user.id].warns += 1;
-
+    // Configure embed
     let embed = new Discord.RichEmbed()
-        .setDescription("Warns")
         .setAuthor(`${client.user.username}'s ModLog`, `${client.user.avatarURL}`)
-        .setColor("#FF4D9C")
-        .setTimestamp()
-        .addField("Warned User", `${user.user.username} with ID ${user.id}`)
-        .addField('Warned By', `<@${message.author.id}> with ID ${message.author.id}`)
-        .addField("Warned In", message.channel)
-        .addField("Number of Warnings", numberW)
-        .addField('Time', message.createdAt)
-        .addField("Reason", reason);
+        .setDescription(`User **${user.user.username}#${user.user.discriminator}** was warned by **${message.author.username}#${message.author.discriminator}** for:\n\n **${reason}**\n\nWarnings: ${res.data.warnings}`)
+        .setColor('#FF4D9C')
+        .setTimestamp();
 
-    if(user) user.send(embed);
+    // Get guild profile
+    const guildRes = await axios.post('http://localhost:8000/guilds/profile', 
+    {'discord_id': message.guild.id, 'name': message.guild.name });
+    const guild = guildRes.data.guild;
 
-    const warnchannel = message.guild.channels.find(c => c.id === settings.modLogChannel)
-    if(!warnchannel) return message.reply(embed);
-    warnchannel.send(embed);
+    // Set channel & check of modlog channel is available. If so, send embed
+    // to the modlog channel, otherwise, send to default message channel
+    if (guild.modLogChannel !== null) {
+        const channel = message.guild.channels.find(c => c.id === guild.modLogChannel);
+        channel.send(embed);
+    }
 
-    if(numberW == settings.warningsMute) {
+    message.channel.send(embed).then(msg => msg.delete(5000));
+
+    if (res.data.warnings === guild.warningsMute) {
         let muterole = message.guild.roles.find(`name`, 'muted')
+
         //check mute role and create
         if(!muterole) return message.channel.send('No mute role, warn has been given but not muted. Please set muted role named "muted"');
 
-        let mutetime = '10s';
+        let mutetime = '120000000s';
+
         await(user.addRole(muterole.id));
+
         message.channel.send(`${user.user.username} has been temporarily muted`).then(msg => {msg.delete(5000)});
 
         setTimeout(function() {
@@ -83,17 +60,16 @@ exports.run = async (client, message, args, level) => {
             message.channel.send(`${user.user.username} has been unmuted`).then(msg => {msg.delete(5000)});
         }, ms(mutetime))
     }
-    if(numberW == settings.warningsBan) {
 
-        const defWarn = {
-            ...warnings,
-            [user.id] : {"warns" : 0}
-        }
-
-        client.settings.set(message.guild.id, defWarn, 'warnings');
+    if(res.data.warnings == guild.warningsBan) {
         message.guild.member(user).ban(reason);
-        message.reply(`${user.username} has been banned`)
+        message.channel.send(`${user.user.username} has been banned for being warned too many times`)
     }
+    } catch (error) {
+        message.channel.send(`Unable to warn user due to an error. If encountered, please send to developers.\n\`${error}\``);
+    }
+
+    
 };
 
 exports.conf = {
