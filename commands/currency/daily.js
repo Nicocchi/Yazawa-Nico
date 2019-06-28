6018412ff89e84c1c1ecce09a6fee11bfedf9d75
Jeremy Boggs <nicocchilover@gmail.com>
2019-06-28 16:39:04 -0400

    send command updated

--- commands/currency/daily.js
+++ commands/currency/daily.js
@@ -9,14 +9,12 @@ exports.run = async (client, message, args, level) => {
     // Set the rip
     const res = await axios.post('http://localhost:8000/users/daily', {'discord_id': message.author.id, 'name': message.guild.name, 'rip': 1 });
     const user = res.data;
-    console.log(user);
 
     // Send message
     message.channel.send(user.message);
 
   } catch (error) {
-    console.log(error.response.message);
-    message.channel.send(`Unable to receive Love Gems due to an error. If encountered, please send to developers. (!support to get invite link) \n\`[${moment().utc()}] Daily | ${error}\``);
+    message.channel.send(`Unable to receive Love Gems due to an error. If encountered, please send to developers. (!support to get invite link) \n\`[${moment().utc()}] Daily | ${error.response}\``);
   }
 };
 
