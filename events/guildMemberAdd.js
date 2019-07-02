5c617c57d083fd12022b872edecf27a444364af8
Jeremy Boggs <nicocchilover@gmail.com>
2019-07-01 22:46:36 -0400

    implemented background changing on profile and greeting

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -22,40 +22,74 @@ const applyText = async (canvas, text, fntSize, weight = "normal") => {
 
 module.exports = async (client, member) => {
   // Load the guild's settings
-  console.log(member.guild);
+  // console.log(member.guild);
   try {
+    const guildRes = await axios.post('http://localhost:8000/guilds/profile', 
+    {'discord_id': member.guild.id, 'name': member.guild.name });
+  const guild = guildRes.data.guild;
+  // console.log(member.guild.members.array().length);
+
     const channel = member.guild.channels.find(ch => ch.name === 'room');
 
-    const canvas = Canvas.createCanvas(700, 250);
+    const canvas = Canvas.createCanvas(1000, 300);
       const ctx = canvas.getContext('2d');
 
-      const background = await Canvas.loadImage(path.resolve(__dirname, `../wallpaper.jpg`));
-      ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+      // const background = await Canvas.loadImage(path.resolve(__dirname, `../wallpaper.jpg`));
+      const bgImage = await Canvas.loadImage(guild.greetingImage);
+      ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
 
       ctx.strokeStyle = '#74037b';
       ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
       // Slightly smaller text placed above the member's display name
-      ctx.font = '48px sans-serif';
+      ctx.font = 'bold 60px sans-serif';
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 8;
+      ctx.strokeText('Welcome', 270, 120);
       ctx.fillStyle = '#ffffff';
-      ctx.fillText('Welcome ', canvas.width / 2, canvas.height / 2.3);
+      ctx.fillText('Welcome ', 270, 120);
 
       // Add an exclamation point here and below
-      ctx.font = applyText(canvas, `${member.displayName}!`);
+      ctx.font = applyText(canvas, `${member.displayName}!`, 60, 'bold');
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 8;
+      ctx.strokeText(`${member.displayName}`, 560, 120);
       ctx.fillStyle = '#ffffff';
-      ctx.fillText(`${member.displayName}`, canvas.width / 2, canvas.height / 1.3);
+      ctx.fillText(`${member.displayName}`, 560, 120);
+
+      ctx.font = applyText(canvas, `to`, 60, 'bold');
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 8;
+      ctx.strokeText(`to`, 270, 180);
+      ctx.fillStyle = '#ffffff';
+      ctx.fillText(`to`, 270, 180);
+
+      ctx.font = applyText(canvas, `${member.guild.name}!`, 60, 'bold');
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 8;
+      ctx.strokeText(`${member.guild.name}`, 340, 180);
+      ctx.fillStyle = '#ffffff';
+      ctx.fillText(`${member.guild.name}`, 340, 180);
+
+      ctx.font = applyText(canvas, `You are the xxxth member!`, 60, 'bold');
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 8;
+      ctx.strokeText(`You are the ${member.guild.members.array().length}th member!`, 270, 250);
+      ctx.fillStyle = '#ffffff';
+      ctx.fillText(`You are the ${member.guild.members.array().length}th member!`, 270, 250);
 
       ctx.beginPath();
-      ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+      ctx.arc(145, 145, 100, 0, Math.PI * 2, true);
       ctx.closePath();
       ctx.clip();
 
       const avatar = await Canvas.loadImage(member.user.displayAvatarURL);
-      ctx.drawImage(avatar, 25, 25, 200, 200);
+      ctx.drawImage(avatar, 45, 45, 245, 245);
 
       const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
+      const msg = guild.welcomeMessage.replace("<user>", member).replace("<guild>", member.guild.name);
 
-      channel.send(`Welcome to the server, ${member}!`, attachment);
+      channel.send(msg, attachment);
     
   } catch (e) {
     client.logger.error(e);
