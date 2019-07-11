786a2e5d40a8d03a7ea6039790982c30a049fa4f
Jeremy Boggs <nicocchilover@gmail.com>
2019-07-11 15:14:55 -0400

    implemented support and invite commands

--- client/commands/core/helpdm.js
+++ client/commands/core/helpdm.js
@@ -1,12 +1,13 @@
 //  Description: Displays all the available commands for your permission level.
 //  Usage: prefix arg1 arg2
 const Discord = require("discord.js");
+const axios = require('axios');
+const moment = require('moment');
 
 exports.run = async (client, message, args, level) => {
-  const defaults = client.config.defaultSettings;
-  if (!client.settings.has(message.guild.id))
-    client.settings.set(message.guild.id, defaults);
-  const settings = client.settings.get(message.guild.id);
+  const guildRes = await axios.post('http://localhost:8000/guilds/profile', 
+    {'discord_id': message.guild.id, 'name': message.guild.name });
+  const guild = guildRes.data.guild;
 
   let embed = new Discord.RichEmbed()
     .setAuthor(`${client.user.username}'s Commands`, `${client.user.avatarURL}`)
@@ -19,13 +20,13 @@ exports.run = async (client, message, args, level) => {
       `Server member since ${message.member.joinedAt}`,
       message.author.displayAvatarURL
     )
-    .addField("Prefix", `${settings.prefix}`, true)
+    .addField("Prefix", `${guild.prefix}`, true)
     .addField(
       "Support | Invite",
       `[Support](https://discord.gg/cs9Sv8N) | [Invite](https://discordapp.com/oauth2/authorize?client_id=506839796921139203&scope=bot&permissions=2146827775)`,
       true
     )
-    .addField(`Version`, `v3.1`, true)
+    .addField(`Version`, `v3.5`, true)
     .addField(`Core`, "`help`, `helpdm`", false)
     .addField(
       `Images`,
@@ -34,12 +35,13 @@ exports.run = async (client, message, args, level) => {
         "`slap`, `sleepy`, `tantrum`, `washi`, `wasted`, `wow`, `yes`",
       false
     )
-    .addField(`Currency`, "`daily`, `profile`, `send`", false)
+    .addField(`Currency`, "`daily`, `send`", false)
     .addField(
       `Fun`,
       "`choose`, `cucumber`, `divorce`, `give`, `marry`, `notice`, `rip`, `say`, `senpai`, `wisdom`",
       false
     )
+    .addField(`Profile`, "`profile`, `background`, `avatar`, `afk`", false)
     .addField(`Games`, "`8ball`, `rps`", false)
     .addField(`Moderation`, "`ban`, `clear`, `kick`, `set`, `warn` ", false)
     .addField(
@@ -52,11 +54,6 @@ exports.run = async (client, message, args, level) => {
       "`adminrole`, `get`, `leave`, `leavechannel`, `leavemessage`, `level`,`modlog`, " +
         "`modlogchannel`, `modrole`, `prefix`, `warningsban`, `warningsmute`, `welcome`, `welcomechannel`, " +
         "`welcomemessage` "
-    )
-    .addField(
-      `Info`,
-      "`afk`, `avatar`, `botinfo`, `userinfo`, `serverinfo`",
-      false
     );
 
   message.author.send(embed);
