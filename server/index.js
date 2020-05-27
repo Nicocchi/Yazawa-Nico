21b228852ec778e82af37eb15cf7c1337dee9e07
Nicocchi <nicocchilover@gmail.com>
2020-05-27 02:30:46 -0400

    converted server to https

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const app = require('./app');
+const fs = require('fs');
+const https = require('https');
+
+const port = process.env.PORT || 8000;
+
+https.createServer({
+    key: fs.readFileSync('yazawanico_fun.key'),
+    cert: fs.readFileSync('yazawanico_fun.cert')
+}, app).listen(port, function () {
+    console.log(`Server listening at ${port}`);
+})
 
 // Start the server
-const port = process.env.PORT || 8000;
-app.listen(port);
-console.log(`Server listening at ${port}`);
+// const port = process.env.PORT || 8000;
+// app.listen(port);
+// console.log(`Server listening at ${port}`);
