117b9d2171a0429aaece1bb8112bf409e89ba866
Nicocchi <nicocchilover@gmail.com>
2020-05-27 03:22:19 -0400

    added ca-bundle with https and changed port to 443

--- server/index.js
+++ server/index.js
@@ -3,14 +3,16 @@ const fs = require('fs');
 const https = require('https');
 
 const port = process.env.PORT || 8000;
+const httpsPort = 443;
 
-https.globalAgent.options.ca = fs.readFileSync('yazawanico_fun.ca-bundle', 'ascii');
+const ca = fs.readFileSync('yazawanico_fun.ca-bundle', 'ascii')
+const cert = fs.readFileSync('yazawanico_fun.crt', 'ascii');
+const key = fs.readFileSync('yazawanico_fun.key', 'ascii')
 
-https.createServer({
-    key: fs.readFileSync('yazawanico_fun.key', 'ascii'),
-    cert: fs.readFileSync('yazawanico_fun.crt', 'ascii')
-}, app).listen(port, function () {
-    console.log(`Server listening at ${port}`);
+const httpsOptions = {cert, ca, key};
+
+https.createServer(httpsOptions, app).listen(httpsPort, function () {
+    console.log(`Server listening at ${httpsPort}`);
 })
 
 // Start the server
