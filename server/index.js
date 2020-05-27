4e61c81f084134a0be98d833f9f8feccffb7c3d2
Nicocchi <nicocchilover@gmail.com>
2020-05-27 03:44:39 -0400

    testing nginx

--- server/index.js
+++ server/index.js
@@ -2,20 +2,20 @@ const app = require('./app');
 const fs = require('fs');
 const https = require('https');
 
-const port = process.env.PORT || 8000;
-const httpsPort = 443;
+const port = process.env.PORT || 8080;
+// const httpsPort = 443;
 
-const ca = fs.readFileSync('yazawanico_fun.ca-bundle', 'ascii')
-const cert = fs.readFileSync('yazawanico_fun.crt', 'ascii');
-const key = fs.readFileSync('yazawanico_fun.key', 'ascii')
+// const ca = fs.readFileSync('yazawanico_fun.ca-bundle', 'ascii')
+// const cert = fs.readFileSync('yazawanico_fun.crt', 'ascii');
+// const key = fs.readFileSync('yazawanico_fun.key', 'ascii')
 
-const httpsOptions = {cert, ca, key};
+// const httpsOptions = {cert, ca, key};
 
-https.createServer(httpsOptions, app).listen(httpsPort, function () {
-    console.log(`Server listening at ${httpsPort}`);
-})
+// https.createServer(httpsOptions, app).listen(httpsPort, function () {
+//     console.log(`Server listening at ${httpsPort}`);
+// })
 
 // Start the server
 // const port = process.env.PORT || 8000;
-// app.listen(port);
-// console.log(`Server listening at ${port}`);
+app.listen(port);
+console.log(`Server listening at ${port}`);
