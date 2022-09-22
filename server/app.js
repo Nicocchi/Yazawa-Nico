aa1820b59c96078aa23b144af72fc0eabe542dbc
Jeremy Boggs <nicocchilover@gmail.com>
2022-09-22 02:38:41 -0400

    fixed issue where server db doesn't start from .env

--- server/app.js
+++ server/app.js
@@ -1,15 +1,18 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 mongoose.Promise = global.Promise;
-if (process.env.NODE_ENV === 'test') {
-    mongoose.connect('mongodb://localhost/TEST_YazawaNico', { useNewUrlParser: true });
+if (process.env.NODE_ENV === "test") {
+  mongoose.connect("mongodb://localhost/TEST_YazawaNico", {
+    useNewUrlParser: true,
+  });
+} else {
+  mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 }
-mongoose.connect('mongodb://localhost/YazawaNico', { useNewUrlParser: true });
 
 const app = express();
 
@@ -19,14 +22,14 @@ app.use(cors());
 // if (!process.env.NODE_ENV === 'test') {
 //     app.use(morgan('combined'));
 // }
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
 app.use(bodyParser.json());
 
 // Routes
-app.use('/users', require('./routes/users'));
-app.use('/guilds', require('./routes/guilds'));
-app.use('/globals', require('./routes/globals'));
-app.use('/api/discord', require('./routes/discord'));
+app.use("/users", require("./routes/users"));
+app.use("/guilds", require("./routes/guilds"));
+app.use("/globals", require("./routes/globals"));
+app.use("/api/discord", require("./routes/discord"));
 
 module.exports = app;
